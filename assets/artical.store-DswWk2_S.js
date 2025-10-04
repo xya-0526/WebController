be7192971016d63e08632a77f4de687367c69620
xya-526 <468422680@qq.com>
2025-10-04 19:38:17 +0800

    Updates

---
+++ assets/artical.store-DswWk2_S.js
@@ -0,0 +1,1 @@
+import{a1 as t,a}from"./index-Bgo2cMrw.js";const i=t("artical",()=>({Artical:a({title:"",content:"",category:"",id:void 0,createTime:"",status:void 0})}));export{i as a};
---
+++ assets/production.stroe-CFizOJrI.js
@@ -0,0 +1,1 @@
+import{a1 as o,a as t}from"./index-Bgo2cMrw.js";const i=o("production",()=>({Production:t({name:"",desc:"",image:"",id:void 0,createTime:"",status:void 0})}));export{i as p};
